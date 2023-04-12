cab10c3c2accc4d475770cc23ec2b3dfe396ab82
Ivan <72632071+starhanski@users.noreply.github.com>
2023-04-12 09:32:11 +0300

    Add files via upload

---
+++ lab 07 Legcovagovik/lab 07 Legcovagovik/DropDecorator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_07_Legcovagovik
+{
+    public class DropDecorator : RainDrop
+    {
+        public DropDecorator(RainDrop rainDrop, Color newcolor)
+            : base(rainDrop.X, rainDrop.Y, rainDrop.Size, rainDrop.Color, rainDrop.Speed)
+        {
+            Color = newcolor;
+        }
+
+        public int NewColor { get; set; }
+    }
+
+}
